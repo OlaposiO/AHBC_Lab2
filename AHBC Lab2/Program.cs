3806822a4d96bc91f0bf6831fd5f921770b558d2
Olaposi <ooomishope@yahoo.com>
2019-07-15 13:59:07 -0400

    full lab 2

--- AHBC Lab2/Program.cs
+++ AHBC Lab2/Program.cs
@@ -6,8 +6,59 @@ namespace AHBC_Lab2
     {
         static void Main(string[] args)
         {
-            //this is my first commit
-            Console.WriteLine("hey world");
+            Console.WriteLine("Hello Human. Welcome to the classroom calcuator!" );
+            double rLength = 0, rWidth = 0, rHeight = 0;
+            //bool runAgain = false;
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter the classrooms length.");
+                string numOne = Console.ReadLine();
+                rLength = double.Parse(numOne); 
+
+                Console.WriteLine("What is the room's width?");
+                string numTwo = Console.ReadLine();
+                rWidth = double.Parse(numTwo);
+
+                Console.WriteLine("What is the height of the room?");
+                string numThree = Console.ReadLine();
+                rHeight = double.Parse(numThree);
+
+                Console.WriteLine($"Length = {rLength}" );
+                Console.WriteLine($"Width = {rWidth}");
+                Console.WriteLine($"Height = {rHeight}");
+
+
+                double area = (rWidth * rLength);
+                double perimeter = ((2 * rWidth) + (2 * rLength));
+                double volume = (rHeight * rLength * rWidth);
+                Console.WriteLine($"The area of this room is: {area}");
+                Console.WriteLine($"The perimeter of this room is: {perimeter}");
+                Console.WriteLine($"The Volume of this room is: {volume}");
+
+                Console.WriteLine("That was fun. Would you to measure any other rooms? [Yes/No]");
+                string userInput = Console.ReadLine();
+                //runAgain = bool.Parse(userInput);
+
+
+                
+                if (userInput == "yes" || userInput == "Yes")
+                {
+                    Console.WriteLine("Awesome. Lets do this again!");
+                    //runAgain = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("You either said no, or made a typo! Well we are all set. Have a great day!");
+                    break;
+                }
+
+            }
+            Console.WriteLine("bye Bye!");
+            Console.ReadLine();
         }
     }
 }
